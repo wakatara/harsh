a83a67bbf8aa0fcf1e3de046a774ca2c26bf954e
Daryl Manning <daryl@wakatara.com>
2022-03-22 20:03:33 +0000

    Bumped version for compilation under go 1.18.

--- harsh.go
+++ harsh.go
@@ -59,7 +59,7 @@ func main() {
 		Name:        "Harsh",
 		Usage:       "habit tracking for geeks",
 		Description: "A simple, minimalist CLI for tracking and understanding habits.",
-		Version:     "0.8.15",
+		Version:     "0.8.16",
 		Commands: []*cli.Command{
 			{
 				Name:    "ask",
